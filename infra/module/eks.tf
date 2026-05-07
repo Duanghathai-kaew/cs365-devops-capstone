ce3211dd19ef87e1c979ffa756b7900bccf88f48
Duanghathai-kaew <duanghathaimek@gmail.com>
2026-05-07 14:26:33 +0700

    fix(cd): update eks cluster name

--- infra/module/eks.tf
+++ infra/module/eks.tf
@@ -25,11 +25,11 @@ resource "aws_eks_cluster" "eks" {
 }
 
 # OIDC Provider
-resource "aws_iam_openid_connect_provider" "eks-oidc" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
-  url             = data.tls_certificate.eks-certificate.url
-}
+# resource "aws_iam_openid_connect_provider" "eks-oidc" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
+#   url             = data.tls_certificate.eks-certificate.url
+# }
 
 
 # AddOns for EKS Cluster
