320536847b1f5a5574470f28f37c66d70332ab77
Duanghathai-kaew <duanghathaimek@gmail.com>
2026-05-14 03:53:10 +0700

    format terraform backend configuration

--- infra/eks/backend.tf
+++ infra/eks/backend.tf
@@ -14,13 +14,13 @@ terraform {
       version = "~> 4.0"
     }
   }
-    backend "s3" {
-      bucket         = "cs365-terraform-state-kaew"
-      key            = "eks/terraform.tfstate"
-      region         = "us-east-1"
-      dynamodb_table = "terraform-lock"
-      encrypt        = true
-    }
+  backend "s3" {
+    bucket         = "cs365-terraform-state-kaew"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
